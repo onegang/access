4c2a676515a4b5dbef25d7f4c256183c28647acf
onegang <leethiamchye@gmail.com>
2020-05-13 16:25:36 +0800

    Initial project

---
+++ access-web/src/main/java/org/onegang/access/MainApplication.java
@@ -0,0 +1,14 @@
+package org.onegang.access;
+
+import java.io.IOException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) throws IOException {
+		SpringApplication.run(MainApplication.class, args);
+	}
+}
