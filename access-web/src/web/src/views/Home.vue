e973f12e5e9d6fbf2c8b4c51a37503c9cc7e399f
onegang <leethiamchye@gmail.com>
2020-05-18 18:31:47 +0800

    Add empty forms

--- access-web/src/web/src/views/Home.vue
+++ access-web/src/web/src/views/Home.vue
@@ -3,26 +3,35 @@
     <div>
     </div>
     <WorkflowButtons />
-    <UsersTable />
+    <transition name="slide-left" mode="out-in">
+      <UsersTable v-show="STAGE===0" />
+    </transition>
+    <transition name="slide-left" mode="out-in">
+      <RequestDetails v-show="STAGE===1" />
+    </transition>
+    <transition name="slide-left" mode="out-in">
+      <RequestSummary v-show="STAGE===2" />
+    </transition>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import UsersTable from '../components/home/UsersTable.vue';
+
 import WorkflowButtons from '../components/home/WorkflowButtons.vue';
+import UsersTable from '../components/home/UsersTable.vue';
+import RequestDetails from '../components/home/RequestDetails.vue';
+import RequestSummary from '../components/home/RequestSummary.vue';
 import {mapGetters} from 'vuex';
 
 export default {
   name: 'Home',
   components: {
-    UsersTable,
     WorkflowButtons,
+    UsersTable,
+    RequestDetails,
+    RequestSummary,
   },
   computed: {
-    // stage() {
-    //   return this.$store.getters.WORKFLOW.stage;
-    // },
     ...mapGetters(['STAGE']),
   },
   methods: {
@@ -32,5 +41,25 @@ export default {
 </script>
 
 <style scoped>
-  
+.slide-left-enter-active,
+.slide-left-leave-active,
+.slide-right-enter-active,
+.slide-right-leave-active {
+  transition-duration: 0.5s;
+  transition-property: height, opacity, transform;
+  transition-timing-function: cubic-bezier(0.55, 0, 0.1, 1);
+  overflow: hidden;
+}
+
+.slide-left-enter,
+.slide-right-leave-active {
+  opacity: 0;
+  transform: translate(2em, 0);
+}
+
+.slide-left-leave-active,
+.slide-right-enter {
+  opacity: 0;
+  transform: translate(-2em, 0);
+}
 </style>
