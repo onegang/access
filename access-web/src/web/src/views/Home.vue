c1d7763046943880cb70c68c8af8ec64cc0d964a
onegang <leethiamchye@gmail.com>
2020-05-18 16:47:34 +0800

    Add new request button

--- access-web/src/web/src/views/Home.vue
+++ access-web/src/web/src/views/Home.vue
@@ -2,6 +2,16 @@
   <div class="home">
     <div>
     </div>
+    <v-badge
+        :content="numberOfSelected"
+        :value="numberOfSelected"
+        overlap
+        offset-x="25"
+        offset-y="25"
+      >
+      <v-btn class="ma-2" outlined color="primary" :disabled="disabled"
+        @click="toAccessForm">New Request</v-btn>
+    </v-badge>
     <UsersTable msg="Welcome to My Access"/>
   </div>
 </template>
@@ -9,11 +19,26 @@
 <script>
 // @ is an alias to /src
 import UsersTable from '../components/UsersTable.vue';
+import {mapGetters} from 'vuex';
 
 export default {
   name: 'Home',
   components: {
     UsersTable,
+  },
+  computed: {
+    numberOfSelected() {
+      return this.$store.getters.USERS.filter(user => user.selected).length;
+    },
+    disabled() {
+      return this.numberOfSelected===0;
+    },
+    ...mapGetters(['USERS']),
+  },
+  methods: {
+    toAccessForm() {
+      console.log('here');
+    }
   },
 };
 </script>
